65ac03a7a1cdc85a23ef9a23fa2499d366e6f59e
preeti <pritich0896@gmail.com>
2023-02-06 00:52:55 +0530

    [Preeti]Modified Compare two Lines based on their end Points

--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -18,7 +18,7 @@ namespace LineComparisonProblem
             int x2 = Convert.ToInt32(Console.ReadLine());
             int y2 = Convert.ToInt32(Console.ReadLine());
             var Length1 = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
-            Console.WriteLine("The Lenth Of the Line Is :" + Length1);
+            Console.WriteLine("The Lenth Of the Line is :" + Length1);
 
             Console.WriteLine("Enter a Value Of a1,b1,a2,b2 Coordinate For Line2");
             int a1 = Convert.ToInt32(Console.ReadLine());
@@ -26,18 +26,22 @@ namespace LineComparisonProblem
             int a2 = Convert.ToInt32(Console.ReadLine());
             int b2 = Convert.ToInt32(Console.ReadLine());
             var Length2 = Math.Sqrt((Math.Pow(a1 - a2, 2) + Math.Pow(b1 - b2, 2)));
-            Console.WriteLine("The Lenth Of the Line Is :" + Length2);
+            Console.WriteLine("The Lenth Of the Line is :" + Length2);
 
-            var result = Equals(Length1, Length2);
-            if (result == false)
+            int result = Length1.CompareTo(Length2);
+            if (result > 0)
             {
-                Console.WriteLine("The Lines Are Not Equal");
+                Console.WriteLine("The Length Of Line1 is Greater than Line2");
+            }
+            else if (result < 0)
+            {
+                Console.WriteLine("The Lenght Of Line2 is Greater than Line1");
             }
             else
             {
-                Console.WriteLine("The Lines Are Equal");
+                Console.WriteLine("Both Lines Are Equal");
             }
-                Console.ReadLine();
+            Console.ReadLine();
         }
     }
 }
